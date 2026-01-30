30711c11d7741f7957b1966060cd103021b064e0
OrtelaKrasniqi <ok74374@ubt-uni.net>
2026-01-30 15:46:17 +0100

    tentim..

--- LogIn/process_login.php
+++ LogIn/process_login.php
@@ -4,7 +4,7 @@ session_start();
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 
-if ($username === "admin@hotmail.com" && $password === "123456") {
+if ($username === "admin@hotmail.com" && $password === "Ortela123!") {
     $_SESSION['logged_in'] = true;
     $_SESSION['username'] = $username;
     header("Location: ../Dashboard/index.php");
